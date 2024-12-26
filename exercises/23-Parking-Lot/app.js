6a65a0ba5df2820182009f4ba8c4344edff1b745
Francisco Yuster <yusterfrancisco@gmail.com>
2024-12-26 13:10:14 +0000

    terminado pero a que costo?

--- exercises/23-Parking-Lot/app.js
+++ exercises/23-Parking-Lot/app.js
@@ -6,6 +6,24 @@ let parkingState = [
 ]
 
 // Your code here
-
+function getParkingLotState(estacionamiento){
+  let state = {
+    totalSlots : 0,
+    availableSlots:0,
+    occupiedSlots: 0
+  }
+  for (let i = 0; i< estacionamiento.length; i++){
+    for ( let x = 0; x < estacionamiento[i].length; x++){
+      if (estacionamiento[i][x] === 2) {
+        state.availableSlots += 1;
+        state.totalSlots += 1;
+      } else if (estacionamiento[i][x] === 1){
+        state.occupiedSlots += 1;
+        state.totalSlots += 1;
+      }
+    }
+  }
+    return state
+}
 
 console.log(getParkingLotState(parkingState))
--- exercises/25-Techno-beat/app.js
+++ exercises/25-Techno-beat/app.js
@@ -1,4 +1,22 @@
-// Your code here
+function lyricsGenerator (arrayNotas){
+    let finalString = ""
+    let countOne = 0
+
+        for (let i = 0; i < arrayNotas.length;i++) {
+            if(arrayNotas[i] == 0){
+                finalString += "Boom "
+                countOne = 0
+            } else if (arrayNotas[i] === 1){
+                finalString += "Drop the bass "
+                countOne += 1
+
+                if(countOne === 3) {
+                    finalString += "!!!Break the bass!!! "
+                }
+            }
+        }
+        return finalString;
+}
 
 
 // Don't change anything below this line
